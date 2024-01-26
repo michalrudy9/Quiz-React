9892c0c32d6865d7813793f2d96931b541d6ca25
michalrudy9 <michallrudy9@gmail.com>
2024-01-26 12:24:55 +0100

    Added view quiz results

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,8 +1,8 @@
 import { useCallback, useState } from "react";
 
 import QUESTIONS from "../questions";
-import quizComplete from "../assets/quiz-complete.png";
 import Question from "./Question";
+import Summary from "./Summary";
 
 function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
@@ -23,12 +23,7 @@ function Quiz() {
   );
 
   if (quizIsComplite) {
-    return (
-      <div id="summary">
-        <img src={quizComplete} alt="Trophy icon" />
-        <h2>Quiz Completed!</h2>
-      </div>
-    );
+    return <Summary userAnswers={userAnswers} />;
   }
 
   return (
